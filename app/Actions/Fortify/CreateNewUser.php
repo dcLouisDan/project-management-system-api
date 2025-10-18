7bed956879b2949cf03f86d0340462bdbcd7fb50
Dan Louis Dela Cruz <louisdc002@gmail.com>
2025-10-18 13:13:19 +0800

    Implement user registration restrictions and role assignment; add RolesAndPermissionsSeeder for permission management

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Spatie\Permission\Models\Role;
 
 class CreateNewUser implements CreatesNewUsers
 {
@@ -19,6 +20,11 @@ class CreateNewUser implements CreatesNewUsers
      */
     public function create(array $input): User
     {
+        $userCount = User::count();
+        if ($userCount > 0) {
+            abort(403, 'User registration is disabled. Please contact your administrator.');
+        }
+
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => [
@@ -31,10 +37,20 @@ class CreateNewUser implements CreatesNewUsers
             'password' => $this->passwordRules(),
         ])->validate();
 
-        return User::create([
+        $user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        $adminRole = Role::where('name', 'admin')->first();
+
+        if (!$adminRole) {
+            abort(500, 'Admin role does not exist. Please contact support.');
+        }
+
+        $user->assignRole($adminRole);
+
+        return $user;
     }
 }
---
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolesAndPermissionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // User management permissions
+        $createUserPermission = Permission::firstOrCreate(['name' => 'create user']);
+        $viewUserPermission = Permission::firstOrCreate(['name' => 'view user']);
+        $updateUserPermission = Permission::firstOrCreate(['name' => 'update user']);
+        $deleteUserPermission = Permission::firstOrCreate(['name' => 'delete user']);
+        $restoreUserPermission = Permission::firstOrCreate(['name' => 'restore user']);
+        $assignRoleUserPermission = Permission::firstOrCreate(['name' => 'assign role user']);
+        $listUsersPermission = Permission::firstOrCreate(['name' => 'list users']);
+
+        // Team management permissions
+        $createTeamPermission = Permission::firstOrCreate(['name' => 'create team']);
+        $viewTeamPermission = Permission::firstOrCreate(['name' => 'view team']);
+        $updateTeamPermission = Permission::firstOrCreate(['name' => 'update team']);
+        $deleteTeamPermission = Permission::firstOrCreate(['name' => 'delete team']);
+        $listTeamsPermission = Permission::firstOrCreate(['name' => 'list teams']);
+        $addMemberTeamPermission = Permission::firstOrCreate(['name' => 'add member team']);
+        $removeMemberTeamPermission = Permission::firstOrCreate(['name' => 'remove member team']);
+        $assignProjectTeamPermission = Permission::firstOrCreate(['name' => 'assign project team']);
+
+        // Project management permissions
+        $createProjectPermission = Permission::firstOrCreate(['name' => 'create project']);
+        $viewProjectPermission = Permission::firstOrCreate(['name' => 'view project']);
+        $updateProjectPermission = Permission::firstOrCreate(['name' => 'update project']);
+        $deleteProjectPermission = Permission::firstOrCreate(['name' => 'delete project']);
+        $listProjectsPermission = Permission::firstOrCreate(['name' => 'list projects']);
+        $archiveProjectPermission = Permission::firstOrCreate(['name' => 'archive project']);
+        $assignTeamProjectPermission = Permission::firstOrCreate(['name' => 'assign team project']);
+        $markProjectCompletePermission = Permission::firstOrCreate(['name' => 'mark project complete']);
+        $viewProjectReportPermission = Permission::firstOrCreate(['name' => 'view project report']);
+
+        // Milestone management permissions
+        $createMilestonePermission = Permission::firstOrCreate(['name' => 'create milestone']);
+        $viewMilestonePermission = Permission::firstOrCreate(['name' => 'view milestone']);
+        $updateMilestonePermission = Permission::firstOrCreate(['name' => 'update milestone']);
+        $deleteMilestonePermission = Permission::firstOrCreate(['name' => 'delete milestone']);
+        $listMilestonesPermission = Permission::firstOrCreate(['name' => 'list milestones']);
+        $markMilestoneCompletePermission = Permission::firstOrCreate(['name' => 'mark milestone complete']);
+
+        // Task Permissions
+        $createTaskPermission = Permission::firstOrCreate(['name' => 'create task']);
+        $viewTaskPermission = Permission::firstOrCreate(['name' => 'view task']);
+        $updateTaskPermission = Permission::firstOrCreate(['name' => 'update task']);
+        $deleteTaskPermission = Permission::firstOrCreate(['name' => 'delete task']);
+        $listTasksPermission = Permission::firstOrCreate(['name' => 'list tasks']);
+        $assignTaskPermission = Permission::firstOrCreate(['name' => 'assign task']);
+        $markTaskDonePermission = Permission::firstOrCreate(['name' => 'mark task done']);
+        $commentTaskPermission = Permission::firstOrCreate(['name' => 'comment task']);
+        $reorderTaskPermission = Permission::firstOrCreate(['name' => 'reorder task']);
+
+        // Comment Permissions
+        $createCommentPermission = Permission::firstOrCreate(['name' => 'create comment']);
+        $viewCommentPermission = Permission::firstOrCreate(['name' => 'view comment']);
+        $updateCommentPermission = Permission::firstOrCreate(['name' => 'update comment']);
+        $deleteCommentPermission = Permission::firstOrCreate(['name' => 'delete comment']);
+        $pinCommentPermission = Permission::firstOrCreate(['name' => 'pin comment']);
+        $listCommentsPermission = Permission::firstOrCreate(['name' => 'list comments']);
+
+        // Attachment Permissions
+        $uploadAttachmentPermission = Permission::firstOrCreate(['name' => 'upload attachment']);
+        $downloadAttachmentPermission = Permission::firstOrCreate(['name' => 'download attachment']);
+        $deleteAttachmentPermission = Permission::firstOrCreate(['name' => 'delete attachment']);
+        $renameAttachmentPermission = Permission::firstOrCreate(['name' => 'rename attachment']);
+        $listAttachmentsPermission = Permission::firstOrCreate(['name' => 'list attachments']);
+
+        // Client Permissions
+        $createClientPermission = Permission::firstOrCreate(['name' => 'create client']);
+        $viewClientPermission = Permission::firstOrCreate(['name' => 'view client']);
+        $updateClientPermission = Permission::firstOrCreate(['name' => 'update client']);
+        $deleteClientPermission = Permission::firstOrCreate(['name' => 'delete client']);
+        $listClientsPermission = Permission::firstOrCreate(['name' => 'list clients']);
+        $archiveClientPermission = Permission::firstOrCreate(['name' => 'archive client']);
+
+        // Define Roles and assign permissions
+        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
+        $adminRole->givePermissionTo(Permission::all());
+
+        $projectManagerRole = Role::firstOrCreate(['name' => 'Project Manager']);
+        $projectManagerRole->givePermissionTo([
+            // Project management permissions
+            $createProjectPermission,
+            $viewProjectPermission,
+            $updateProjectPermission,
+            $deleteProjectPermission,
+            $listProjectsPermission,
+            $archiveProjectPermission,
+            $assignTeamProjectPermission,
+            $markProjectCompletePermission,
+            $viewProjectReportPermission,
+            // Milestone management permissions
+            $createMilestonePermission,
+            $viewMilestonePermission,
+            $updateMilestonePermission,
+            $deleteMilestonePermission,
+            $listMilestonesPermission,
+            $markMilestoneCompletePermission,
+            // Task permissions
+            $createTaskPermission,
+            $viewTaskPermission,
+            $updateTaskPermission,
+            $deleteTaskPermission,
+            $listTasksPermission,
+            $assignTaskPermission,
+            $markTaskDonePermission,
+            $commentTaskPermission,
+            $reorderTaskPermission,
+        ]);
+
+        $teamLeadRole = Role::firstOrCreate(['name' => 'Team Lead']);
+        $teamLeadRole->givePermissionTo([
+            // Task permissions
+            $createTaskPermission,
+            $viewTaskPermission,
+            $updateTaskPermission,
+            $deleteTaskPermission,
+            $listTasksPermission,
+            $assignTaskPermission,
+            $markTaskDonePermission,
+            $commentTaskPermission,
+            $reorderTaskPermission,
+        ]);
+
+        $teamMemberRole = Role::firstOrCreate(['name' => 'Team Member']);
+        $teamMemberRole->givePermissionTo([
+            // Task permissions
+            $viewTaskPermission,
+            $listTasksPermission,
+            $markTaskDonePermission,
+            $commentTaskPermission,
+        ]);
+    }
+}
